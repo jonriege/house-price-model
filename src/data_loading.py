b7cefecbc1a47f43894beba123384432cf368a72
Jon Riege <jonriege1996@gmail.com>
2023-03-05 18:05:37 +0100

    Implement parsing of SSB data to DataFrame

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,22 +1,46 @@
 """Loading of data from Statistisk Sentralbyrå"""
 
+import warnings
+
+import pandas as pd
+from pyjstat import pyjstat
 from requests import post
 
 
-def load_ssb_table(config: dict, table: str, query: list[dict]) -> dict:
+def load_ssb_table(config: dict, table: str, query: list[dict]) -> str:
     """Loads data from SSB API.
 
     Args:
-      config: Config
-      table: ID of the table to load.
-      query: Query specifying what data to load from the table
-        (see https://www.ssb.no/en/api/api-eksempler-pa-kode).
+        config: Config
+        table: ID of the table to load.
+        query: Query specifying what data to load from the table
+            (see https://www.ssb.no/en/api/api-eksempler-pa-kode).
 
     Returns:
-        Data returned by SSB API.
+        Data returned by SSB API in string format.
     """
     url = config["data_loading"]["ssb_api_url"] + table
     payload = {"query": query, "response": {"format": "json-stat2"}}
     response = post(url, json=payload)
     response.raise_for_status()
-    return response.json()
+    return response.text
+
+
+def parse_ssb_data(ssb_api_output: str) -> pd.DataFrame:
+    """Parses data from SSB API and converts it to a DataFrame.
+
+    Args:
+        ssb_api_output: JSON-stat formatted data returned by SSB API.
+
+    Returns:
+        Data converted to a pandas DataFrame.
+    """
+    dataset = pyjstat.Dataset.read(ssb_api_output)
+
+    # The pyjstat library has a bug where a DeprecationWarning is raised when
+    # converting the Dataset object to a DataFrame using .write(), although
+    # there is no other way to perform this operation.
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        df = dataset.write("dataframe")
+    return df
--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -1,6 +1,18 @@
 """Test data loading from Statistisk Sentralbyrå"""
+import json
 
-from src.data_loading import load_ssb_table
+import pandas as pd
+import pytest
+
+from src.data_loading import load_ssb_table, parse_ssb_data
+
+
+@pytest.fixture(scope="function")
+def ssb_api_output() -> str:
+    """Loads example output from SSB API."""
+    with open("tests/data/ssb_api_output.json", "r") as f:
+        data = f.read()
+    return data
 
 
 def test_load_ssb_table(config: dict):
@@ -17,4 +29,14 @@ def test_load_ssb_table(config: dict):
         },
     ]
     data = load_ssb_table(config=config, table=table, query=query)
-    assert data["source"] == "Statistisk sentralbyrå"
+    assert isinstance(data, str)
+    data_dict = json.loads(data)
+    assert data_dict["source"] == "Statistisk sentralbyrå"
+
+
+def test_parse_ssb_data(ssb_api_output: str):
+    """Tests parsing of SSB API output into DataFrame."""
+    df = parse_ssb_data(ssb_api_output=ssb_api_output)
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape[0] == 1
+    assert df.loc[0, "value"] == 44599
