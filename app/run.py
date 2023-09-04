8c19f6ab818167b5c7b090185e9da9d4daa9972d
Jon Riege <jonriege1996@gmail.com>
2023-09-04 15:58:44 +0200

    Add templates from AppSeed

--- app/run.py
+++ app/run.py
@@ -1,9 +1,19 @@
 """Run the Flask application."""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main() -> str:
-    return "Hello world!"
+    return render_template("home/index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(_: Exception) -> str:
+    return render_template("home/page-404.html")
+
+
+@app.errorhandler(500)
+def internal_server_error(_: Exception) -> str:
+    return render_template("home/page-500.html")
